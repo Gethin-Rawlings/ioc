14c312ae5c6a98ee5eb4224b66c86e0b9df747a5
Gethin Rawlings <gethin.p.rawlings@gmail.com>
2020-03-24 15:22:04 +0000

    clean up of code

--- src/IEpisodeRepository.ts
+++ src/IEpisodeRepository.ts
@@ -3,8 +3,6 @@ import { Episode } from './Episode';
 
 export interface IEpisodeRepository {
      get() : Episode[];
-     getById(id: number) : Episode;
      add(track: Episode) : number;
      edit(id: number, track: Episode) : Episode;
-     delete(id: number) : Episode;
 }
